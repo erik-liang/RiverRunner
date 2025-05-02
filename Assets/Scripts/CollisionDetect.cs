1ad371b89be2a9ddc8f37e0c47b7e352eee2c28b
erik-liang <68139423+erik-liang@users.noreply.github.com>
2025-05-02 16:47:42 -0700

    create basic logic for start and end screen
    start menu is a separate scene called StartScreen.unity and uses the StartScreenController script to connects to the Game.unity scene.
    
    end menu simply restarts the player to the beginning (stays in the Game.unity scene). Game manager handles and loads the Game.unity scene after restart and the ColliionDetect.cs file is modified to add the restart panel screen.
    
    The following has been done for the current rocks in our segments, but to make this all work for newly created segments, we need to add the Panel object to the restartPanel component in the CollisionDetect script for every obstacle/rock we create from now on.

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    void OnTriggerEnter(Collider other) {
+    [SerializeField] GameObject restartPanel;  // Drag the restart panel here in the Inspector
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Stop the player
         player.GetComponent<PlayerMovement>().enabled = false;
+
+        // Show the Game Over screen
+        restartPanel.SetActive(true);
     }
 }
