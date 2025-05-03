e8b9432a0227fad4b168b59cae8d6bec041be347
erik-liang <68139423+erik-liang@users.noreply.github.com>
2025-05-02 17:29:50 -0700

    Fixes coin count so it resets to 0 after restarting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public void RestartGame()
     {
+        MasterInfo.coinCount = 0;  // 🧹 Reset coin count
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
